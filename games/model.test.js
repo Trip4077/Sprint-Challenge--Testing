1bc70d034ec6dd31a51bb25b68fa48653df2bfab
Trip4077 <bj2336@email.vccs.edu>
2019-04-20 13:02:36 -0400

    getAll test FAIL

--- games/model.test.js
+++ games/model.test.js
@@ -1,3 +1,12 @@
-describe('GAMES', () => {
+const Games = require('./model');
+const db = require('../data/dbConfig');
 
+describe('GAMES', () => {
+    describe('getAll()', () => {
+        it('should return an empty array if no games found', async () => {
+            const games = await Games.getAll();
+
+            expect(games).toEqual([]);
+        });
+    });
 });
